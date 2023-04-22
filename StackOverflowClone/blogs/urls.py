8334de99f2fe12985459e79ece52cca0d296ec32
hemalpatel0303 <hemalpatel0303@gmail.com>
2023-04-22 17:17:02 +0530

    question post edit and view

--- StackOverflowClone/blogs/urls.py
+++ StackOverflowClone/blogs/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("post_question/", views.post_question, name="post_question"),
+    path("post_question/<int:question_id>/", views.post_question, name="post_question"),
+    path("view_question/<int:question_id>/", views.view_question, name="view_question"),
 ]
--- StackOverflowClone/blogs/views.py
+++ StackOverflowClone/blogs/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import QuestionForm
 from django.contrib import messages
+from .models import Post, PostAnswer, Tags
 
 
 # Create your views here.
-def post_question(request):
-    print("request", request.method)
+def post_question(request, question_id):
     try:
         if request.method == "POST":
             form = QuestionForm(request.POST)
@@ -14,7 +14,18 @@ def post_question(request):
                 question.author_id = request.user.id
                 question.save()
                 messages.success(request, "Question has been posted.")
+                return redirect(view_question, question_id=question.id)
+
         form = QuestionForm()
         return render(request, "blogs/post_question.html", context={"form": form})
+    except Exception as e:
+        print("Error", e)
+
+
+def view_question(request, question_id):
+    try:
+        if question_id:
+            Post.objects.filter(id=question_id).values("title", "body").first()
+            return render(request, "blogs/view_question.html", context={})
     except Exception as e:
         print("Error", e)
