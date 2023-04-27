4eab56928b3bc3d573ece9c7aea2c317f40772f8
hemalpatel0303 <hemalpatel0303@gmail.com>
2023-04-28 00:51:08 +0530

    Elastic configed

---
+++ StackOverflowClone/blogs/documents.py
@@ -0,0 +1,21 @@
+from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.registries import registry
+from .models import Post
+
+
+@registry.register_document
+class PostDocument(Document):
+    class Index:
+        # Name of the Elasticsearch index
+        name = "questions"
+
+    class Django:
+        model = Post  # The model associated with this Document
+
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            # your fields here
+            "id",
+            "title",
+            "body",
+        ]
