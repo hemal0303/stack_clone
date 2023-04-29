2bbca82bbb5afc965f7f4966d566616f2f934f24
rathod-vaibhav <vaibhavrathod303@gmail.com>
2023-04-30 00:54:50 +0530

    google auth login.

--- StackOverflowClone/home/urls.py
+++ StackOverflowClone/home/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path,include
 
 from . import views
+
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.authcheck, name="login"),
     path("register/", views.register, name="register"),
     path("signout/", views.signout, name="signout"),
+    path("social-auth/", include('social_django.urls', namespace='social')),
+    path("logout/", auth_view.LogoutView.as_view(),name="logout"),
+
 ]
