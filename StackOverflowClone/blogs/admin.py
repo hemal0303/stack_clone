5489608e42f4fcdc7b691017e8dde7e5d2001a0a
Hemal  Patel <hemalpatel@hemals-air.home>
2023-11-18 18:27:23 -0500

    Removed temp model

--- StackOverflowClone/blogs/admin.py
+++ StackOverflowClone/blogs/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 
-from .models import (
-    Comment,
-    MaintenanceMode,
-    Post,
-    PostAnswer,
-    Tags,
-    Vote,
-    Notification,
-    TempModel,
-)
+from .models import Comment, MaintenanceMode, Post, PostAnswer, Tags, Vote, Notification
 
 admin.site.register(Post)
 admin.site.register(PostAnswer)
@@ -18,4 +9,3 @@ admin.site.register(MaintenanceMode)
 admin.site.register(Vote)
 admin.site.register(Comment)
 admin.site.register(Notification)
-admin.site.register(TempModel)
--- StackOverflowClone/blogs/models.py
+++ StackOverflowClone/blogs/models.py
@@ -162,7 +162,3 @@ class Notification(models.Model):
 
     def __str__(self):
         return str(self.post.title)
-
-
-class TempModel(models.Model):
-    body = models.TextField()
